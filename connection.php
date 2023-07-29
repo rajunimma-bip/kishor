4f6dca3df8628f392b1a90e87bb733d394d87dee
Kishor L <75676692+kishor-23@users.noreply.github.com>
2023-07-29 13:52:35 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,7 +1,7 @@
 
 
-<?
+<?php
 //change mysqli_connect(host_name,username, password); 
-$connection = mysqli_connect("localhost:3307", "root", "");
+$connection = mysqli_connect("localhost", "root", "");
 $db = mysqli_select_db($connection, 'demo');
 ?>
